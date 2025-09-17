8cdf4ef4129e32863b9579e48102dcba92f7010e
Rajeev Kumar Kaushal <rajeevkumarkaushal8563@gmail.com>
2025-09-17 23:10:14 +0530

    error handling

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    // Disable ESLint during production builds
     ignoreDuringBuilds: true,
-  },
-  typescript: {
-    // Disable TypeScript errors during production builds (use with caution)
-    ignoreBuildErrors: false,
   },
 }
 
